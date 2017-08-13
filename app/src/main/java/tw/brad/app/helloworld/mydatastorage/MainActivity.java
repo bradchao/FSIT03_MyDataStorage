27978b927ecf44e06b5aef7e7b863f6a7ba0c013
Chien <chien@gmail.com>
2017-08-13 13:28:08 +0800

    v2

--- app/src/main/java/tw/brad/app/helloworld/mydatastorage/MainActivity.java
+++ app/src/main/java/tw/brad/app/helloworld/mydatastorage/MainActivity.java
@@ -1,7 +1,12 @@
 package tw.brad.app.helloworld.mydatastorage;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }else{
+            init();
+        }
+
+    }
+
+    private void init(){
         sp = getSharedPreferences("gamedata", MODE_PRIVATE);    // gamedata.xml
         editor = sp.edit();
 
@@ -33,10 +51,22 @@ public class MainActivity extends AppCompatActivity {
         if (!approot.exists()){
             approot.mkdirs();
         }
-
-
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 123){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                init();
+            }else{
+                finish();
+            }
+        }
+    }
+
     public void test1(View view){
         editor.putString("username", "Brad");
         editor.putInt("stage", 2);
@@ -72,4 +102,12 @@ public class MainActivity extends AppCompatActivity {
             Log.i("brad", e.toString());
         }
     }
+    public void test5(View view){
+        
+    }
+    public void test6(View view){
+
+    }
+
+
 }
